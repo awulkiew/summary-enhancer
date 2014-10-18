6008608c1c46796f804c3dd2f0b955aa7faa78e5
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2014-10-18 02:33:11 +0200

    Download logs asynchroniously

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 
 #include <fstream>
 #include <iostream>
+#include <vector>
 
 #include <boost/filesystem.hpp>
 #include <boost/algorithm/string/regex.hpp>
@@ -82,36 +83,179 @@ inline void set_value(rapidxml::xml_document<> & doc, NorA * n, const char* v)
         n->first_node("")->value(cstr);
 }
 
-inline void modify_nodes(rapidxml::xml_document<> & doc, rapidxml::xml_node<> * td, rapidxml::xml_node<> * a, std::string const& log_url)
+struct fail_node
 {
-    BOOST_ASSERT(td && a && !log_url.empty());
+    fail_node(rapidxml::xml_node<> * td_,
+              rapidxml::xml_node<> * a_,
+              rapidxml::xml_attribute<> * href_,
+              std::string const& log_url_)
+        : td(td_), a(a_), href(href_), log_url(log_url_)
+    {}
+    rapidxml::xml_node<> * td;
+    rapidxml::xml_node<> * a;
+    rapidxml::xml_attribute<> * href;
+    std::string log_url;
+};
 
-    std::cout << "Processing: " << log_url.substr(log_url.find_last_of('/') + 1) << std::endl;
-    
-    std::string error_msg;
-    std::string log = get_document(log_url, error_msg);
+struct fail_nodes
+    : public std::vector<fail_node>
+{
+    typedef std::vector<fail_node> base_t;
 
-    if ( !error_msg.empty() )
+    fail_nodes(rapidxml::xml_document<> & doc)
     {
-        std::cerr << "ERROR! " << error_msg << std::endl;
-        return;
+        gather_nodes(doc.first_node());
     }
 
+private:
+    inline void gather_nodes(rapidxml::xml_node<> * n)
+    {
+        if ( n == NULL )
+            return;
+
+        // "fail" <td>
+        if ( "td" == name(n) )
+        {
+            rapidxml::xml_attribute<> * attr = n->first_attribute("class");
+
+            if ( attr && "library-fail-unexpected-new" == value(attr) )
+            {
+                // "fail" <a>
+                rapidxml::xml_node<> * anch = n->first_node("a");
+                if ( anch )
+                {
+                    rapidxml::xml_attribute<> * href_attr = anch->first_attribute("href");
+                    if ( href_attr )
+                    {
+                        // "fail link"
+                        std::string href = value(href_attr);
+
+                        // make global URL from local URL
+                        if ( href.find("http://") == std::string::npos )
+                        {
+                            // remove prefixing '/'
+                            while ( !href.empty() && href[0] == '/' )
+                            {
+                                href.erase(href.begin());
+                            }
+
+                            href = branch_url + href;
+                        }
+
+                        this->push_back(fail_node(n, anch, href_attr, href));
+                    }
+                }
+            }
+        }
+
+        // depth first
+        gather_nodes(n->first_node());
+        gather_nodes(n->next_sibling());
+    }
+};
+
+struct logs_pool
+{
+    typedef boost::network::http::basic_client<boost::network::http::tags::http_async_8bit_udp_resolve, 1, 1> client_type;
+    typedef std::vector<fail_node>::iterator fail_iterator;
+    
+    struct element
+    {
+        element(fail_iterator it_, client_type::response const& response_)
+            : fail_it(it_), response(response_), is_active(true)
+        {}
+
+        fail_iterator fail_it;
+        client_type::response response;
+        bool is_active;
+    };
+
+    struct log_info
+    {
+        log_info(fail_iterator it_, std::string const& log_)
+            : fail_it(it_), log(log_)
+        {}
+
+        fail_iterator fail_it;
+        std::string log;
+    };
+
+    typedef std::vector<element>::iterator response_iterator;
+
+    logs_pool(std::size_t max_requests_ = 10)
+        : max_requests(max_requests_)
+    {}
+
+    fail_iterator add(fail_iterator first, fail_iterator last)
+    {
+        for ( ; first != last && responses.size() < max_requests ; ++first )
+        {
+            client_type::request request(first->log_url);
+            request << boost::network::header("Host", "www.boost.org");
+            request << boost::network::header("Connection", "keep-alive");
+            
+            responses.push_back(element(first, client.get(request)));
+        }
+
+        return first;
+    }
+
+    template <typename OutIt>
+    void get(OutIt out)
+    {
+        for ( response_iterator it = responses.begin() ; it != responses.end() ; ++it )
+        {
+            if ( boost::network::http::ready(it->response) )
+            {
+                it->is_active = false;
+
+                std::string body;
+
+                try
+                {
+                    body = boost::network::http::body(it->response);
+                }
+                catch (std::exception & e)
+                {
+                    std::cerr << "ERROR! " << e.what() << std::endl;
+                }
+
+                *out++ = log_info(it->fail_it, body);
+            }
+        }
+
+        response_iterator it = std::remove_if(responses.begin(), responses.end(), is_not_active);
+        responses.erase(it, responses.end());
+    }
+
+    static bool is_not_active(element const& el) { return !el.is_active; }
+
+    std::size_t max_requests;
+    client_type client;
+    std::vector<element> responses;
+};
+
+inline void modify_nodes(rapidxml::xml_document<> & doc,
+                         fail_node & n,
+                         std::string const& log)
+{
+    std::cout << "Processing: " << n.log_url.substr(n.log_url.find_last_of('/') + 1) << std::endl;
+    
     // time limit exceeded
     if ( log.find("second time limit exceeded") != std::string::npos )
     {
         rapidxml::xml_attribute<> * style_attr = doc.allocate_attribute("style", "background-color: #88ff00;");
-        td->append_attribute(style_attr);
+        n.td->append_attribute(style_attr);
 
-        set_value(doc, a, "time");
+        set_value(doc, n.a, "time");
     }
     // File too big
     else if ( log.find("File too big") != std::string::npos )
     {
         rapidxml::xml_attribute<> * style_attr = doc.allocate_attribute("style", "background-color: #00ff88;");
-        td->append_attribute(style_attr);
+        n.td->append_attribute(style_attr);
 
-        set_value(doc, a, "file");
+        set_value(doc, n.a, "file");
     }
     // internal compiler error
     else if ( ! boost::empty( boost::algorithm::find_regex(
@@ -121,9 +265,9 @@ inline void modify_nodes(rapidxml::xml_document<> & doc, rapidxml::xml_node<> * 
                 )) )
     {
         rapidxml::xml_attribute<> * style_attr = doc.allocate_attribute("style", "background-color: #ff88ff;");
-        td->append_attribute(style_attr);
+        n.td->append_attribute(style_attr);
 
-        set_value(doc, a, "ierr");
+        set_value(doc, n.a, "ierr");
     }
     // compilation failed
     else if ( ! boost::empty( boost::algorithm::find_regex(
@@ -133,9 +277,9 @@ inline void modify_nodes(rapidxml::xml_document<> & doc, rapidxml::xml_node<> * 
                 )) )
     {
         rapidxml::xml_attribute<> * style_attr = doc.allocate_attribute("style", "background-color: #ffbb00;");
-        td->append_attribute(style_attr);
+        n.td->append_attribute(style_attr);
 
-        set_value(doc, a, "comp");
+        set_value(doc, n.a, "comp");
     }
     // linking failed
     else if ( ! boost::empty( boost::algorithm::find_regex(
@@ -145,9 +289,9 @@ inline void modify_nodes(rapidxml::xml_document<> & doc, rapidxml::xml_node<> * 
                 )) )
     {
         rapidxml::xml_attribute<> * style_attr = doc.allocate_attribute("style", "background-color: #ffdd00;");
-        td->append_attribute(style_attr);
+        n.td->append_attribute(style_attr);
 
-        set_value(doc, a, "link");
+        set_value(doc, n.a, "link");
     }
     // run failed
     else if ( ! boost::empty( boost::algorithm::find_regex(
@@ -156,75 +300,45 @@ inline void modify_nodes(rapidxml::xml_document<> & doc, rapidxml::xml_node<> * 
                     boost::match_not_dot_newline
                 )) )
     {
-        set_value(doc, a, "run");
+        rapidxml::xml_attribute<> * style_attr = doc.allocate_attribute("style", "background-color: #ffff00;");
+        n.td->append_attribute(style_attr);
+
+        set_value(doc, n.a, "run");
     }
     // unknown fail
     else
     {
-        set_value(doc, a, "unkn");
+        rapidxml::xml_attribute<> * style_attr = doc.allocate_attribute("style", "background-color: #ffff88;");
+        n.td->append_attribute(style_attr);
+
+        set_value(doc, n.a, "unkn");
     }
 }
 
-inline void process_node(rapidxml::xml_document<> & doc, rapidxml::xml_node<> * n)
+inline void process_fail(rapidxml::xml_document<> & doc, fail_node & n, std::string const& log)
 {
-    if ( n == NULL )
-        return;
-
-    // "fail" <td>
-    if ( "td" == name(n) )
+    // remove spaces
+    while ( n.td->first_node("") )
     {
-        rapidxml::xml_attribute<> * attr = n->first_attribute("class");
-
-        if ( attr && "library-fail-unexpected-new" == value(attr) )
-        {
-            // remove spaces
-            while ( n->first_node("") )
-            {
-                n->remove_node(n->first_node(""));
-            }
-
-            // "fail" <a>
-            rapidxml::xml_node<> * anch = n->first_node("a");
-            if ( anch )
-            {
-                rapidxml::xml_attribute<> * href_attr = anch->first_attribute("href");
-                if ( href_attr )
-                {
-                    // "fail link"
-                    std::string href = value(href_attr);
-
-                    if ( href.find("BP") != std::string::npos )
-                    {
-                        int a = 10;
-                    }
-
-                    // make global URL from local URL
-                    if ( href.find("http://") == std::string::npos )
-                    {
-                        // remove prefixing '/'
-                        while ( !href.empty() && href[0] == '/' )
-                        {
-                            href.erase(href.begin());
-                        }
-
-                        href = branch_url + href;
-
-                        char * href_cstr = doc.allocate_string(href.c_str());
-                        href_attr->value(href_cstr);
-                    }
-
-                    modify_nodes(doc, n, anch, href);
-                }
-            }
-        }
+        n.td->remove_node(n.td->first_node(""));
     }
 
-    // depth first
-    process_node(doc, n->first_node());
-    process_node(doc, n->next_sibling());
+    // set new, global href
+    n.href->value( doc.allocate_string(n.log_url.c_str()) );
+
+    modify_nodes(doc, n, log);
 }
 
-inline void process_document(std::string & in, std::string & out)
+inline void print_log_urls(fail_nodes::iterator first, fail_nodes::iterator last)
+{
+    // Print
+    for ( ; first != last ; ++first )
+    {
+        std::cout << "Downloading: " << first->log_url.substr(first->log_url.find_last_of('/') + 1) << std::endl;
+    }
+}
+
+inline void process_document(std::string const& name, std::string & in, std::string & out)
 {
     out.clear();
     if ( in.empty() )
@@ -233,7 +347,34 @@ inline void process_document(std::string & in, std::string & out)
     rapidxml::xml_document<> doc;
     doc.parse<0>(&in[0]); // non-98-standard but should work
 
-    process_node(doc, doc.first_node());
+    fail_nodes nodes(doc);
+    logs_pool pool(20);
+
+    fail_nodes::iterator it = nodes.begin();
+    
+    while ( it != nodes.end() || !pool.responses.empty() )
+    {
+        // new portion
+        fail_nodes::iterator new_it = pool.add(it, nodes.end());
+
+        print_log_urls(it, new_it);
+        it = new_it;
+
+        // wait a while
+        boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+
+        // get downloaded logs
+        std::vector<logs_pool::log_info> logs;
+        pool.get(std::back_inserter(logs));
+
+        for ( std::vector<logs_pool::log_info>::iterator log_it = logs.begin() ;
+              log_it != logs.end() ; ++log_it )
+        {
+            process_fail(doc, *(log_it->fail_it), log_it->log);
+        }
+    }
+
+    std::cout << "Saving: " << name << std::endl;
 
     rapidxml::print(std::back_inserter(out), doc);
 }
@@ -300,7 +441,7 @@ int main(int argc, char **argv)
         std::string lib = argv[i];
         std::string url = view_url + lib + "_.html";
 
-        std::cout << "Processing: " << lib << std::endl;
+        std::cout << "Downloading: " << lib << std::endl;
         
         // download the summary page
         std::string error_msg;
@@ -314,7 +455,8 @@ int main(int argc, char **argv)
 
         // process the summary page
         std::string processed_body;
-        process_document(body, processed_body);
+
+        process_document(lib, body, processed_body);
 
         // save processed summary page
         std::ofstream of(std::string("result/") + lib + ".html", std::ios::trunc);
